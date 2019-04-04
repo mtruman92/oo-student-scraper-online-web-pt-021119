6b18a588fbb5d03ef3724304f8b2b1c0d7dd1415
Mickae Truman <mtruman@nyit.edu>
2019-04-04 16:38:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,9 +14,9 @@ class Scraper
       student_list << {
         :name => list.css("h4.student-name").text,
         :location => list.css("p.student-location").text,
-        :profile_url => "./fixtures/student-site/index.html" + list.css("a").attribute("href").value
         binding.pry
-              }
+        :profile_url => "./fixtures/student-site/index.html" + list.css("a").attribute("href").value
+                      }
         end
         student_list
     
