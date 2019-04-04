f72db58783a53bbd3c417966d68f29f95346bb13
Mickae Truman <mtruman@nyit.edu>
2019-04-04 16:21:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,15 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    #binding.pry
+    html = open(index_url)
+    index = Nokogiri::HTML
+    #(html(open("http://142.93.56.73:60992/fixtures/student-site/"))
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
