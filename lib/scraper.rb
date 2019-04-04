d4e365a298535ca9c43185299800d338c5d8e5d1
Mickae Truman <mtruman@nyit.edu>
2019-04-04 16:23:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,23 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    #binding.pry
-    html = open(index_url)
-    index = Nokogiri::HTML
-    #(html(open("http://142.93.56.73:60992/fixtures/student-site/"))
-    binding.pry
-  end
+    student_list = []
+        html = open(index_url)
+      Nokogiri::HTML(open("http://142.93.56.73:34389/fixtures/student-site/"))
+    list = Nokogiri::HTML(html)
+    list.css(".student-card").collect do |student|
+      student_list << {
+        :name => list.css("h4.student-name").text,
+        :location => list.css("p.student-location").text,
+        :profile_url => "./fixtures/student-site/index.html" + list.css("a").attribute("href")
+      }
+        end
+        student_list
 
   def self.scrape_profile_page(profile_url)
     
   end
 
 end
+
 
